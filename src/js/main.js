2294382f2d2fce59ff6f501724041a7e14638749
Bryan Besnyi <besnyib@smccd.edu>
2024-06-13 12:30:05 -0700

    Initial Commit

---
+++ src/js/main.js
@@ -0,0 +1,41 @@
+import * as bootstrap from 'bootstrap';
+
+const navbar = `
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="#">Navbar</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+            aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="#">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Features</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Pricing</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link disabled">Disabled</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
+`;
+
+const footer = `
+<footer class="footer mt-auto py-3 bg-light">
+    <div class="container">
+        <span class="text-muted
+            ">Place sticky footer content here.</span>
+    </div>
+</footer>
+`;
+
+document.getElementById('arias-nav').innerHTML = navbar;
+document.getElementById('arias-footer').innerHTML = footer;
